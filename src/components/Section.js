c3699156617411662247d9193c689c0e02f8a2f5
8xNehanSS <nehan.sudasinghe@gmail.com>
2024-04-01 18:50:58 +0530

    Update Section.js

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,22 +1,26 @@
 import React from "react";
-import Card from './Card';
+import Card from "./Card";
 
-const Section = ((props) => {
+const Section = (props) => {
+  const data = props.books;
+  const search = props.searchTerm;
+  //map here
+  console.log(data);
+  const Cards = data.map((book) => {
+    return <Card books={book} />;
+  });
 
-    const data = props.data.results.books;
-    console.log(data);
-    
-    //map here
-    const Cards = data.map((book) => {
-        return <Card books={book}/>
-    })
-    
-    return (
-        <section>
-            <h2 className="section--heading">Fiction</h2>
-            <div className="section">{Cards.slice(0,8)}</div>
-        </section> 
-    )
-})
+  return (
+    <section>
+      <h2
+        className="section--heading"
+        style={!search ? { visibility: "hidden" } : null}
+      >
+        Search Term: {search}
+      </h2>
+      <div className="section">{Cards.slice(0, 19)}</div>
+    </section>
+  );
+};
 
 export default Section;
