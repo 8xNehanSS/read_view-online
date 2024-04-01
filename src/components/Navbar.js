b2cb8597f51bd79559846f9615d30fbabf8b4cea
8xNehanSS <nehan.sudasinghe@gmail.com>
2024-04-01 18:50:02 +0530

    Update navbar for further development

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,20 @@
 import React from "react";
 
-const Navbar = (()=> {
-    return (
-        <nav>
-            <img src='./images/logo.png' alt="logo" />
-            <h1>Read View - Online</h1>
-            <div className="nav--right">
-                <h3>Login</h3>
-                <h3>Sign Up</h3>
-            </div>
-        </nav>
-    )
-})
+const Navbar = () => {
+  return (
+    <nav>
+      <img src="./images/logo.png" alt="logo" />
+      <h1>Read View - Online</h1>
+      <div className="nav--right">
+        <div>
+          <h3>Login</h3>
+        </div>
+        <div>
+          <h3>Sign Up</h3>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
