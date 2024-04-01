182ce3b5361d00a97b4952aa8eeab9d68ccf4d81
8xNehanSS <nehan.sudasinghe@gmail.com>
2024-04-01 19:19:06 +0530

    Added routes for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
       <img src="./images/logo.png" alt="logo" />
-      <h1>Read View - Online</h1>
+      <Link to="/">
+        <h1>Read View - Online</h1>
+      </Link>
       <div className="nav--right">
-        <div>
-          <h3>Login</h3>
-        </div>
-        <div>
-          <h3>Sign Up</h3>
-        </div>
+        <Link to="/login">
+          <div>
+            <h3 className="link--normal">Login</h3>
+          </div>
+        </Link>
+        <Link to="/signup">
+          <div>
+            <h3 className="link--normal">Sign Up</h3>
+          </div>
+        </Link>
       </div>
     </nav>
   );
